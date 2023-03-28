ac5fc78356516a6fcf01218ab5f261d05130ad53
jacek <s24881@pjwstk.edu.pl>
2023-03-28 18:45:31 +0200

    rzeczy zrobione

---
+++ apbd3/Controllers/StudentsController.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+using apbd3.Models;
+using Microsoft.AspNetCore.Mvc;
+using CsvHelper;
+
+namespace apbd3.Controllers;
+
+[ApiController]
+[Route("students")]
+public class StudentsController : ControllerBase
+{
+    [HttpGet("names")]
+    public string GetNames()
+    {
+        return "students names";
+    }
+
+    [HttpGet("{idStudent}/grades")]
+    public string GetStudentGrades(int idStudent)
+    {
+        return idStudent + " grades: 1,2,3";
+    }
+
+    [HttpGet("{idStudent}")]
+    public IActionResult GetStudent(int idStudent)
+    {
+        var student = new Student()
+        {
+            numerIndeksu = "s1000",
+            imie = "Jacke",
+            nazwisko = "Gorsky"
+        };
+        return Ok(student);
+    }
+
+    [HttpPost("addStudent")]
+    public IActionResult CreateStudent(Student newStudent)
+    {
+        List<Student> list;
+        using (var reader = new StreamReader("Resources/students.csv"))
+        using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            list = csvReader.GetRecords<Student>().ToList();
+        }
+
+        using (var writer = new StreamWriter("Resources/students.csv"))
+        using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            list.Add(newStudent);
+            csvWriter.WriteRecords(list);
+        }
+
+        return Ok("gituwa");
+    }
+
+    [HttpGet]
+    public IActionResult GetAllStudents()
+    {
+        using (var reader = new StreamReader("Resources/students.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            return Ok(csv.GetRecords<Student>().ToList());
+        }
+    }
+    
+}
---
+++ apbd3/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace apbd3.Models;
+
+public class Student
+{
+    //Imie,nazwisko,numerIndeksu,dataUrodzenia,studia,tryb,email,imię ojca, imię matki
+    public string imie { get; set; }
+    public string nazwisko { get; set; }
+    public string numerIndeksu { get; set; }
+    public string dataUrodzenia { get; set; }
+    public string studia { get; set; }
+    public string tryb { get; set; }
+    public string email { get; set; }
+    public string imieOjca { get; set; }
+    public string imieMatki { get; set; }
+}
